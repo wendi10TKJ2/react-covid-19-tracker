e17a1033eab71cc93851b345351410a80364754a
wendtbrias <wendtbrias@gmail.com>
2022-07-24 21:51:06 +0700

    second commit

--- src/helper/formatCount.js
+++ src/helper/formatCount.js
@@ -1,5 +1,7 @@
-export default function formatCount(int = 0) {
-  if (typeof int === "number") {
+export default function formatCount(int) {
+  if (int !== null && typeof int === "number") {
     return int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+
+  return 0;
 }
